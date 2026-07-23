82de34eea3da86023404de0e4c0f610b074dec4c
DRACKWARS <127371302+DRACKWARS@users.noreply.github.com>
2026-07-23 19:05:13 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,3 +1,8 @@
 <?php
-// Pastikan ada DUA garis bawah sebelum dan sesudah kata DIR
+// Tambahkan 3 baris ini untuk debugging paksa
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Sisa kode Anda di bawahnya...
 require __DIR__ . '/../public/index.php';
