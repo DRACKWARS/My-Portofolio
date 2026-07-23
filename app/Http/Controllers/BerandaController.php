cd07850638429e5cabe54a12e2d1397849c080cc
DRACKWARS <127371302+DRACKWARS@users.noreply.github.com>
2026-07-23 20:27:08 +0700

    Update BerandaController.php

--- app/Http/Controllers/BerandaController.php
+++ app/Http/Controllers/BerandaController.php
@@ -12,6 +12,7 @@ class BerandaController extends Controller
     {
         $projects = [
             (object) [
+                'id' => 1,
                 'name'=>'Book Rental System',
                 'about'=>'Readify is a web-based book rental application that allows users to browse, borrow, and return books online. It also provides an administrative dashboard for managing books, users, categories, and rental transactions efficiently.',
                 'key_takeaways'=>'Responsive UI with Bootstrap|MVC Architecture Implementation|Laravel CRUD Development|User Authentication & Authorization',
@@ -20,6 +21,7 @@ class BerandaController extends Controller
                 'thumbnail'=>'beranda.png'
             ],
             (object) [
+                'id' => 2,
                 'name'=>'Smart Cattle Monitoring System',
                 'about'=>'An IoT-based Smart Cattle Monitoring System developed using Laravel to monitor environmental conditions and cattle behavior in real time. The system collects data from multiple sensors, including temperature, humidity, ammonia (NH3), carbon monoxide (CO), light intensity, and motion sensors. The collected data is transmitted from an ESP8266 microcontroller to the Laravel backend, where it is visualized through interactive dashboards, historical data tables, charts, and Temperature Humidity Index (THI) analysis to support better livestock management and decision-making.',
                 'key_takeaways'=>'Designed and developed a full-stack IoT monitoring web application using Laravel MVC Architecture|Integrated ESP8266 with multiple environmental sensors through HTTP API communication|Implemented real-time monitoring for temperature, humidity, NH3, CO, light intensity, and cattle behavior|Built interactive dashboards with historical sensor data visualization and charts|Implemented Temperature Humidity Index (THI) analysis to evaluate livestock environmental conditions|Developed RESTful APIs for seamless communication between IoT devices and the Laravel backend|Managed relational databases using MySQL and Eloquent ORM',
