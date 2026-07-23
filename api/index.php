71968fdf93dab1ce3fe95d301dcb433d177a7ff6
DRACKWARS <127371302+DRACKWARS@users.noreply.github.com>
2026-07-23 19:12:32 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,8 +1,3 @@
 <?php
-// Tambahkan 3 baris ini untuk debugging paksa
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-// Sisa kode Anda di bawahnya...
-require __DIR__ . '/../public/index.php';
+echo "<h1>Vercel PHP Menyala!</h1>";
+exit;
