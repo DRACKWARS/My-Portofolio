43b6966782848eae8c0fd9384849a38e766f4b33
DRACKWARS <dionpratama153@gmail.com>
2026-07-23 22:30:10 +0700

    penambahan foto project

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -176,7 +176,9 @@
             <div class="col-md-4">
                 <div class="custom-card p-3">
                     <div class="bg-white rounded mb-3 overflow-hidden" style="height: 220px;">
-                        <img src="{{(https://0ih3lxtivqmpf53e.public.blob.vercel-storage.com/images/beranda.png) }}"
+                        <img src="{{ asset('images/'.$image->image) }}
+                                    {{ $image->image }}
+                                    {{  $project->thumbnail}}"
                             alt="{{ $project->name }}"
                             class="w-100 h-100"
                             style="object-fit: cover;">
